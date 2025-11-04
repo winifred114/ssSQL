ce05a31adc7c4096b5b18ecfcb9b20fb3d803aa8
winifred114 <e7418741877@gmail.com>
2025-11-04 23:31:10 +0900

    sql1 실습

---
+++ sql1/db_hw_1_4/db_hw_1_4.sql
@@ -0,0 +1,17 @@
+SELECT *
+FROM tracks
+WHERE name LIKE '%love%';
+
+SELECT *
+FROM tracks
+WHERE "GenreId" = 1 AND "Milliseconds" >= 300000
+ORDER BY "UnitPrice" DESC;
+
+SELECT "GenreId", COUNT(*) AS TotalTracks
+FROM tracks
+GROUP BY "GenreId";
+
+SELECT "GenreId", SUM(UnitPrice) AS TotalPrice
+FROM tracks
+GROUP BY "GenreId"
+HAVING TotalPrice >= 100;
