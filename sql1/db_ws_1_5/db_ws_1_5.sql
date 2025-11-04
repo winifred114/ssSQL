89d09334ce3b1dc5968513a5f1ac1a58b2074f46
winifred114 <e7418741877@gmail.com>
2025-11-04 23:10:02 +0900

    sql1 실습

---
+++ sql1/db_ws_1_5/db_ws_1_5.sql
@@ -0,0 +1,40 @@
+SELECT *
+FROM users
+WHERE age >= 30 AND balance >= 1000;
+
+SELECT *
+FROM users
+WHERE balance <= 1000 AND age <= 20;
+
+SELECT *
+FROM users
+WHERE first_name LIKE '현%' AND country = '제주특별자치도'
+ORDER BY age DESC
+LIMIT 1;
+
+SELECT *
+FROM users
+WHERE last_name = '박' AND age >= 25
+ORDER BY age
+LIMIT 1;
+
+SELECT *
+FROM users
+WHERE first_name IN ('재은', '영일')
+ORDER BY balance DESC
+LIMIT 1;
+
+SELECT *
+FROM users
+GROUP BY country
+HAVING max(balance)
+ORDER BY balance DESC;
+
+SELECT *
+FROM users
+WHERE age >= 30
+    AND balance > (
+        SELECT AVG(balance)
+        FROM users
+        WHERE age >= 30
+    );
